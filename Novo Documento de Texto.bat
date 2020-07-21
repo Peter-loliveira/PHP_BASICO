63195a161dea229c6e5439680d1c5959f64922e5
peter-loliveira <peter.loliveira@gmail.com>
2020-07-21 14:29:28 -0300

    index.php

---
+++ Novo Documento de Texto.bat
@@ -0,0 +1,6 @@
+echo "# PHP_BASICO_UDEMY" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/Peter-loliveira/PHP_BASICO_UDEMY.git
+git push -u origin master
