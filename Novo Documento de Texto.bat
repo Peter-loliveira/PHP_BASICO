59468028847c0668af0796eebe4105642238eff9
peter-loliveira <peter.loliveira@gmail.com>
2020-07-23 08:16:49 -0300

    Desisti do Curso.Uma merda

--- Novo Documento de Texto.bat
+++
@@ -1,6 +0,0 @@
-echo "# PHP_BASICO_UDEMY" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/Peter-loliveira/PHP_BASICO_UDEMY.git
-git push -u origin master
--- index.php
+++ index.php
@@ -1,35 +1,5 @@
-<?php 
-$mensagem = "CALCULOS\n";
+<?php
 
-$a = 10;
-$b = 37;
-$br = "<br><br>";
+phpinfo();
 
-echo "WHILE:<br>";
-$i = 0;
-while ($i <= $a) {
-    print $i;
-    $i++;
-}
-
-echo $br;
-echo "DO .. WHILE:<br>";
-$i = 0;
-do {
-    print $i;
-    $i++;
-} while ($i <= $a);
-
-echo $br;
-echo "FOR: <br>";
-for ($i=0; $i <= $a; $i++) { 
-    print $i;
-}
-
-echo $br;
-echo "FOREACH: <br>";
-$i = [0,1,2,3,4,5,6,7,8,9,10];
-foreach ($i as $j) {
-    print $j;
-}
-
+?>
