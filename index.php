808c2a30d45fe55a3c869f3e275f57738588c9ba
peter-loliveira <peter.loliveira@gmail.com>
2020-07-23 12:41:25 -0300

    Primeira aula concluida

--- index.php
+++ index.php
@@ -1,10 +1,50 @@
 <?php
+$QuebraDeLinha = "<br><br>";
 
-$br = "<br>";
-$teste = [0,1,2,3,4,5,6,7,8,9,10];
-foreach ($teste as $j) {
-    print $j;
-    echo $br;
+$categorias = [];
+$categorias[] = "Infantil";
+$categorias[] = "Adolescente";
+$categorias[] = "Adulto";
+
+
+// var_dump($idade);
+// echo $QuebraDeLinha;
+// var_dump($nome);
+// echo $QuebraDeLinha;
+// echo $QuebraDeLinha;
+
+
+for ($i=0; $i <= 2; $i++) { 
+    echo $categorias[$i];
+    echo $QuebraDeLinha;
 }
 
-?>
+echo $QuebraDeLinha;
+$idade = 16;
+$nome = "Peter";
+echo $QuebraDeLinha;
+echo "O nadador ".$nome." tem ".$idade. " anos";
+echo $QuebraDeLinha;
+
+echo "LAÇO FOR";
+for ($i=0; $i < count($categorias) ; $i++) { 
+    echo "O nadador " .$nome. " pertence à categoria " .$categorias[$i];
+    echo $QuebraDeLinha;
+}
+echo $QuebraDeLinha;
+
+echo "LAÇO foreach";
+echo $QuebraDeLinha;
+foreach ($categorias as $key) {
+    echo "O nadador " .$nome. " pertence à categoria " .$key;
+    echo $QuebraDeLinha;
+    
+}
+
+// if ($idade >= 6 && $idade <=12) {
+//     echo $categorias[0];
+// } elseif ($idade > 12 && $idade < 18) {
+//     echo $categorias[1];
+// } else {
+//     echo $categorias[2];
+// }
