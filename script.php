ac5647d2a4f1e58bdc4d23fc31063a0aa94c1577
peter-loliveira <peter.loliveira@gmail.com>
2020-07-25 09:54:23 -0300

    Ajustes na pagina de do Script para incluir HTML e CSS

--- script.php
+++ script.php
@@ -17,37 +17,67 @@ echo "<br>"; // Pula uma linha APENAS para o texto não ficar colado no inicio d
 if (empty($nome) || empty($idade)) { //empty() verifica se uma variável está vazia
 	$_SESSION['ErroValidacaoDados'] = "AMBOS os campos devem ser preenchidos!";
 	header("location: index.php");
+	return;
 } else {
 	// 2º - Verifica se o valor digitado no campo idade é um número
 	if (!is_numeric($idade)) {
 		$_SESSION['ErroValidacaoDados'] = "O valor digitado DEVE ser um número.";
 		header("location: index.php");
+		return;
 	} else {
 		// 3º - Verifica se o campo NOME tem pelo menos 2 e não mais de 40 caracteres
 		if (strlen($nome) < 2) {			//strlen() CONTA quantos caracteres a variável tem
 			$_SESSION['ErroValidacaoDados'] = "O Nome deve conter pelo menos 2 caracteres.";
 			header("location: index.php");
+			return;
 		} elseif (strlen($nome) > 40) {
 			$_SESSION['ErroValidacaoDados'] = "O Nome não pode conter mais de 40 caracteres.";
 			header("location: index.php");
+			return;
 		} else {
 			// 4º - Verifica se a idade do competidor é compatível com a competição
 			if ($idade > 100) {
 				$_SESSION['ErroValidacaoDados'] = "É vedada a participação de competidores ACIMA dos 100 anos!<br>QUER MATAR O(A) VELHINHO(A)!";
 				header("location: index.php");
+				return;
 			} elseif ($idade < 6) {
 				//A principio não seria necessário isso, pois o input do HTML é do tipo NUMBER. Mas como usuário tem parte com o cão....
 				$_SESSION['ErroValidacaoDados'] = "É vedada a participação de competidores ABAIXO dos 6 anos!";
 				header("location: index.php");
+				return;
 			} else {
 				if ($idade >= 6 && $idade < 12) {
-					echo "O competidor " . $nome . " tem " . $idade . " anos e é da categoria " . $categorias[0] . ".";
+					$SUCESSO = "O competidor " . $nome . " tem " . $idade . " anos e é da categoria " . $categorias[0] . ".";
 				} elseif ($idade >= 12 && $idade < 18) {
-					echo "O competidor " . $nome . " tem " . $idade . " anos e é da categoria " . $categorias[1] . ".";
+					$SUCESSO = "O competidor " . $nome . " tem " . $idade . " anos e é da categoria " . $categorias[1] . ".";
 				} elseif ($idade >= 18) {
-					echo "O competidor " . $nome . " tem " . $idade . " anos e é da categoria " . $categorias[2] . ".";
+					$SUCESSO = "O competidor " . $nome . " tem " . $idade . " anos e é da categoria " . $categorias[2] . ".";
 				}
+				session_destroy();
 			}
 		}
 	}
 }
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>PHP - Formulários de Envio</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+	<h4 class="text-danger text-center">
+	<?php
+	  echo $SUCESSO;
+	?>
+	</h4>
+
+</body>
+</html>
